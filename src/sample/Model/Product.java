0126a3f9c8465468115804d5f77f33f7f06d56bc
Andrew Lee <ajlee2006@gmail.com>
2021-08-12 22:18:55 +0800

    a

--- src/sample/Model/Product.java
+++ src/sample/Model/Product.java
@@ -1,4 +1,44 @@
 package sample.Model;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.io.*;
+
 public class Product {
+    private static ArrayList<Product> products;
+    private String productID, name, description, brand, country;
+    private double price;
+    private int temperature, weight;
+    private char size;
+    private Date date;
+    private ArrayList<String> images;
+
+    public Product(String productID, String name, String description, String brand, double price, int temperature, char size, String country, String date, int weight, ArrayList<String> images) {
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+            this.date = sdf.parse(date);
+        } catch (ParseException e) {
+            throw new IllegalArgumentException("Invalid date (dd-MM-yyyy)");
+        }
+        if (!productID.matches("^(FD|BV|PM)\\d{5}$")) throw new IllegalArgumentException("Invalid product ID");
+        char us = Character.toUpperCase(size); // just in case
+        if (us != 'S' && us != 'M' && us != 'L') throw new IllegalArgumentException("Invalid size (S|M|L)");
+
+        this.productID = productID;
+        this.name = name;
+        this.description = description;
+        this.brand = brand;
+        this.price = price;
+        this.temperature = temperature;
+        this.size = us;
+        this.country = country;
+        this.weight = weight;
+        this.images = images;
+    }
+
+    public Product(String productID, String name, String description, String brand, double price, int temperature, char size, String country, String date, int weight, String image) {
+        // just in case
+        this(productID,name,description,brand,price,temperature,size,country,date,weight,new ArrayList<String>(Arrays.asList(image)));
+    }
 }
--- src/sample/Model/Security.java
+++ src/sample/Model/Security.java
@@ -1,4 +1,61 @@
 package sample.Model;
 
+import java.util.*;
+import java.io.*;
+
 public class Security {
+    private static ArrayList<Security> users;
+    private static final String FILENAME = "Login.csv";
+    private String userID, password;
+
+    public Security(String userID, String password) {
+        if (!validUserID(userID)) throw new IllegalArgumentException("Invalid user ID");
+        if (!validPassword(password)) throw new IllegalArgumentException("Invalid password");
+        this.userID = userID;
+        this.password = password;
+    }
+
+    public static ArrayList<Security> getUsers() {
+        return users;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public static void loadLoginDB() {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(FILENAME));
+            String s;
+            do {
+                s = br.readLine();
+                if (s != null) {
+                    String[] tokens = s.split(",");
+                    users.add(new Security(tokens[0],tokens[1]));
+                }
+            } while (s != null);
+        } catch (Exception e) {}
+    }
+
+    public static boolean authenticate(String userID, String password) {
+        loadLoginDB();
+        if (!validUserID(userID)) throw new IllegalArgumentException("Invalid user ID");
+        if (!validPassword(password)) throw new IllegalArgumentException("Invalid password");
+        for (int i=0;i<users.size();i++) {
+            if (users.get(i).getUserID().equals(userID) && users.get(i).getPassword().equals(password)) return true;
+        }
+        return false;
+    }
+
+    public static boolean validUserID(String userID) {
+        return userID.matches("^(AD|BA)\\d{5}$");
+    }
+
+    public static boolean validPassword(String password) {
+        return password.matches("^[A-Za-z0-9]{6,}$");
+    }
 }
